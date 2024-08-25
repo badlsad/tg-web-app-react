c02cfeaee63cd5203952567c586a41f23974de96
Aidar Yeskaliyev <1badlsad1@gmail.com>
2024-08-25 16:13:27 +0500

    Add a few components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { useEffect } from 'react';
 import './App.css';
+import { useEffect } from 'react';
+
+import Header from './components/Header/Header';
 
 const tg = window.Telegram.WebApp;
+
 function App() {
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
-
   return (
     <div className="App">
-      work
-      <button onClick={onClose}>X</button>
+      <Header />
+      <div>lol</div>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        src="https://www.creativefabrica.com/wp-content/uploads/2021/11/09/Coffee-shop-vintage-logo-design-Graphics-19876672-1-1-580x386.png"
+        alt=""
+        className="logo_img"
+      />
+    </div>
+  );
+};
+
+export default Header;
