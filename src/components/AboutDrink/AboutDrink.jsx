cc9314326dc4019f88a30fdc592dd0d1ac7867ad
Aidar Yeskaliyev <1badlsad1@gmail.com>
2024-08-25 19:03:33 +0500

    added db

--- src/components/AboutDrink/AboutDrink.jsx
+++ src/components/AboutDrink/AboutDrink.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './AboutDrink.css';
 import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
 
-const AboutDrink = () => {
+const AboutDrink = (db) => {
+  const data = db.coffee;
+
+  const url = window.location.href;
+  const regex = /\/about\/id=(\d+)/;
+  const match = url.match(regex);
+  const id = match[1];
+
+  const drink = data[id];
+
   return (
-    <>
-      <div className="name">Lavanda</div>
-      <div className="mainText">Lavand is pid</div>
-      <Link to="/">Back</Link>
-    </>
+    <div className="about">
+      <h2 className="aboutName">{drink.name}</h2>
+      <img src={drink.image} alt={drink.name} className="aboutImg" />
+      <h3 className="aboutTitle">Ингредиенты</h3>
+      <ul className="aboutIngrList">
+        {drink.ingredients.map((ingredient) => (
+          <li className="aboutIngredient">{ingredient}</li>
+        ))}
+      </ul>
+      <h3 className="aboutTitle">Приготовление</h3>
+      <div className="aboutText">{drink.description}</div>
+      <Link to="/" className="text-link">
+        <Button className="redBtn">Назад</Button>
+      </Link>
+    </div>
   );
 };
 
--- src/components/CoffeeItems/CoffeeItems.jsx
+++ src/components/CoffeeItems/CoffeeItems.jsx
@@ -3,19 +3,18 @@ import Button from '../Button/Button';
 
 import './CoffeeItems.css';
 import { Link } from 'react-router-dom';
-const CoffeeItems = () => {
+
+const CoffeeItems = (db) => {
+  const data = db.coffee;
   return (
     <div className="coffeeItems">
       <h4 className="coffeeItems_category">Кофе☕</h4>
       <div className="coffeeItems_wrapper">
-        <Link to="/about/0">
-          <Button className="coffeeItem">Лавандовый раф</Button>
-        </Link>
-        <Button className="coffeeItem">Лимонный раф</Button>
-        <Button className="coffeeItem">Кокосовый раф</Button>
-        <Button className="coffeeItem">Латте</Button>
-        <Button className="coffeeItem">Капуччино</Button>
-        <Button className="coffeeItem">Айс-латте</Button>
+        {data.map((item) => (
+          <Link to={`/about/id=${item.id}`} key={item.id} className="text-link">
+            <Button className="coffeeItem">{item.name}</Button>
+          </Link>
+        ))}
       </div>
     </div>
   );
